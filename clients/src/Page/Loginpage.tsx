d86698fda0ca3afada3d5a756aeff2be06165438
naphattar <naphatchalermwai@gmail.com>
2023-01-02 23:38:56 +0700

    initialize template

---
+++ clients/src/Page/Loginpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Loginpage() {
+  return (
+    <div className="loginpage">
+      
+    </div>
+  );
+}
+
+export default Loginpage;
---
+++ clients/src/Page/Selectpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Selectpage() {
+  return (
+    <div className="Selectpage">
+      
+    </div>
+  );
+}
+
+export default Selectpage;

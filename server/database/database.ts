6d2cecd39b2bdbe1b7377426609d99c286175e5f
naphattar <naphatchalermwai@gmail.com>
2023-01-05 23:21:23 +0700

    fixed database url bug

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,14 +1,11 @@
 import mongoose, { ConnectOptions, mongo } from "mongoose";
-const dotenv = require("dotenv");
 
-dotenv.config();
+require("dotenv").config();
 
-// mock databaseurl untill find the way to use dotebv with typescript
-const databaseUrl : string = "mongodb+srv://naphattar:raklungkeaw@gungao.x6oje7p.mongodb.net/?retryWrites=true&w=majority";
-//const databaseUrl : string =   `${process.env.MONGO_URL}`||"mongodb+srv://naphattar:raklungkeaw@gungao.x6oje7p.mongodb.net/?retryWrites=true&w=majority";
+const databaseUrl : string =   `${process.env.MONGO_URL}`;
 
 exports.connect = () =>{
-    console.log(databaseUrl);
+
     //database connecting
     mongoose.connect(databaseUrl, {
         useNewUrlParser: true,

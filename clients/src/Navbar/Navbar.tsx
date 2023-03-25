67d13af5dfb5f13f841a2a9495b603fb4ee48947
naphattar <naphatchalermwai@gmail.com>
2023-03-26 02:10:23 +0700

    update homepage design

--- clients/src/Navbar/Navbar.tsx
+++ clients/src/Navbar/Navbar.tsx
@@ -4,8 +4,13 @@ import Loginbutton from './Loginbutton';
 import "./Navbar.css"
 import Navitem from './Navitem';
 import { User } from '../Interfaces/User';
+import iconimage from "./assets/brandicon.png";
+import { useNavigate } from 'react-router-dom';
+
 function Navbar() {
   const [isLogin,setIslogin] = useState(false);
+  const [toggle,setToggle] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(()=>{
     AuthService.getCurrentUser()
@@ -17,18 +22,19 @@ function Navbar() {
       setIslogin(false);
     })
   },[]);
-
   return (
     <nav className="navbar">
-      <p className="navbrand">
-        GUngao1.1
-      </p>
+      <div className="navbrand">
+        <img src={iconimage} onClick={()=>{navigate("/")}}></img>
+        <p onClick={()=>{navigate("/")}}>GUngao1.1</p>
+      </div>
       <ul className="navlist">
         <Navitem item={"Home"} tolink={"/"}></Navitem>
         <Navitem item={"How to use"} tolink={"/howtouse"}></Navitem>
         <Navitem item={"About us"} tolink={"/aboutus"}></Navitem>
       </ul>
       <Loginbutton islogin={isLogin}/>
+      <button onClick={() => {setToggle(true)}} className="togglebutton">Open Menu</button>
     </nav>
   );
 }
--- clients/src/Page/Homepage/Homepage.tsx
+++ clients/src/Page/Homepage/Homepage.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import backgroundimage from "./assets/Homepagebackground.jpg";
 import "./Mainpage.css"
 function Homepage() {
+  const navigate = useNavigate();
+
   return (
-    <div className="homepagebody">
-      <div className="homepagetext">
-        <div className="homepagetext1">
-          <p>เหงาปาก เหงาใจ</p>
-          <p>ไร้เพื่อนคุย</p>
-        </div>
-        <div className="homepagetext2">
-          <p>ให้เราหา "เพื่อนคุย" ให้กับคุณ</p>
-        </div>
+    <div className="homepagebody"  id="homepagebackground"
+    style={{backgroundImage: `url(${backgroundimage})`}}
+    >
+      <div className="homepagetextcontainer">
+        <p>เหงาปาก เหงาใจ ไร้เพื่อนคุย</p>
+        <p>ให้เราหา "เพื่อนคุย" ให้กับคุณ</p>
+        <button onClick={()=> {navigate("/register")}}>เข้าร่วมกับเรา</button>
       </div>
     </div>
   );
